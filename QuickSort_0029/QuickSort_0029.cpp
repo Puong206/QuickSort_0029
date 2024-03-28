8678fcccb04892cfc7b17db349435e68f4e324a1
Puong206 <bagasrf206@gmail.com>
2024-03-28 17:31:43 +0700

    Void Display

--- QuickSort_0029/QuickSort_0029.cpp
+++ QuickSort_0029/QuickSort_0029.cpp
@@ -78,6 +78,20 @@ void q_short(int low, int high)
     q_short(j + 1, high);
 }
 
+void display() {
+    cout << "\n---------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "---------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of Comparasions : " << cmp_count << endl;
+    cout << "Number of Data Movements : " << mov_count << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
